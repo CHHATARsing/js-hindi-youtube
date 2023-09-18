4bc3e641c1d6ec8bffff5811ad5c5137f973a221
CHHATARsing <140980174+CHHATARsing@users.noreply.github.com>
2023-09-18 19:57:54 +0000

    add new file of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12334;
+let accountEmail = "ravisoni@gmail.com";
+var password = "2344545";
+accountCity = "delhi";
+let accountState;
+
+//accountId = 2;
+
+// console.log(accountId);//not allowe
+
+accountEmail = "sv.@gmail.com";
+password = "et44";
+accountCity = "noida";
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, password, accountCity, accountState]);
