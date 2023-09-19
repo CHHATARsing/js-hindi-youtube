ae3f5a34bd453334da312f5a2118daf6dfd17258
CHHATARsing <140980174+CHHATARsing@users.noreply.github.com>
2023-09-19 16:30:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "Ravi";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(score);
+// console.log(typeof score);
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedIn = "ravi";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ravi" => ture
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
