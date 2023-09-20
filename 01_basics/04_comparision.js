32c6b5216734ab2b0b4a824e88bc8658b160e857
CHHATARsing <140980174+CHHATARsing@users.noreply.github.com>
2023-09-20 12:22:33 +0000

    tutoral for conversion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <  1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// this type of conversion and equal check can be confues  
+// === strict check
+
+console.log("2" === 2);
