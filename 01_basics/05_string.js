74328c3623e89f20ce5f05cbd8ddab6e0f831e32
CHHATARsing <140980174+CHHATARsing@users.noreply.github.com>
2023-10-04 11:58:16 +0000

    string file

---
+++ 01_basics/05_string.js
@@ -0,0 +1,15 @@
+// const name = "chhatar";
+// console.log(name.length);
+
+// const surname = new String("Sing");
+// console.log(surname.length);
+
+// const text = "ASHDHDLHOHAHEIHEWHKLAOIHERHOIHREHHDK";
+// const length = text.length;
+// console.log(length);
+
+
+//++++++++++++++ javaScript String slice()++++++++++++++
+
+
+
